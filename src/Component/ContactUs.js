3999ae502732842b2feaea034772e7fe9af3c025
Marc WU <32423253+wumarc@users.noreply.github.com>
2023-06-09 21:39:37 -0400

    c

--- src/Component/ContactUs.js
+++ src/Component/ContactUs.js
@@ -64,7 +64,7 @@ const ContactUs = () => {
                 <div>
                     <div class="fs-2 fw-bold mb-2">Our Location</div>
                     
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2799.9863531271885!2d-75.69122242323446!3d45.42977643573416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05031f5b834b%3A0x18c70ed203b29f53!2sHair%20Tango!5e0!3m2!1sen!2sca!4v1686197082856!5m2!1sen!2sca" width="320" height="200" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
+                    <iframe title="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2799.9863531271885!2d-75.69122242323446!3d45.42977643573416!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4cce05031f5b834b%3A0x18c70ed203b29f53!2sHair%20Tango!5e0!3m2!1sen!2sca!4v1686197082856!5m2!1sen!2sca" width="320" height="200" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/>
                     
                     <div className="d-flex flex-row">
                         <i class="fa fa-location-arrow" style={{fontSize:"22px"}}></i>
--- src/Component/NoPage.js
+++ src/Component/NoPage.js
@@ -1,7 +1,3 @@
-import NavBar from "./NavBar";
-
-
-
 const NoPage = () => {
 
     return (
